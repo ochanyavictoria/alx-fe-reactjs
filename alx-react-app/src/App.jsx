cad4fa335aa4d18970430276a688c1683f34c5ea
ochanyavictoria <157648941+ochanyavictoria@users.noreply.github.com>
2025-08-28 13:53:02 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,16 @@
-import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <WelcomeMessage />
-    </div>
-  )
+    return (
+        <div className="App">
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
