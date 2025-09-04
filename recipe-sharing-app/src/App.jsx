5f20f9c674107e50b0473f19cfb130a6debe6180
ochanyavictoria <157648941+ochanyavictoria@users.noreply.github.com>
2025-09-04 14:43:49 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
-import AddRecipeForm from "../components/AddRecipeForm";
-import RecipeList from "../components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
 
 function App() {
   return (
