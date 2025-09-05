d81eaec8d993a42a8373398336eaf11ae02a9ec0
ochanyavictoria <christophervictoria07@gmail.com>
2025-09-05 14:54:28 +0100

    Add GitHub user search functionality with styled results

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,10 @@
-import ExampleComponent from "./components/ExampleComponent";
-import { testService } from "./services/api";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
       <h1>GitHub User Search</h1>
-      <ExampleComponent />
-      <p>{testService()}</p>
+      <Search />
     </div>
   );
 }
