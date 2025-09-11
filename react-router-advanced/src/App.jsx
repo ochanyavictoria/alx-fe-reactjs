b743fd22ee96a415a8653a7c7141fbcd08fc2cc2
ochanyavictoria <christophervictoria07@gmail.com>
2025-09-11 14:58:56 +0100

    updated app

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,20 +1,21 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./components/Home";
-import BlogPost from "./components/BlogPost";
-import Profile from "./components/Profile";
-import Login from "./components/Login";
+import Home from "./pages/Home";
+import BlogPost from "./pages/BlogPost";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Home */}
         <Route path="/" element={<Home />} />
 
-        {/* Dynamic route for blog posts */}
-        <Route path="/posts/:id" element={<BlogPost />} />
+        {/* Dynamic routing for blog posts */}
+        <Route path="/blog/:id" element={<BlogPost />} />
 
-        {/* Protected route wrapper */}
+        {/* Protected Profile routes (nested inside Profile.jsx) */}
         <Route
           path="/profile/*"
           element={
@@ -24,11 +25,11 @@ function App() {
           }
         />
 
-        {/* Fallback route */}
-        <Route path="*" element={<Navigate to="/" />} />
-
         {/* Login */}
         <Route path="/login" element={<Login />} />
+
+        {/* Catch-all route */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
